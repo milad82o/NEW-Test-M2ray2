7a344ece6cac110a28ca357a53e87a206e00c7e0
milad82o <milad82mi@hotmail.com>
2026-05-16 20:53:41 +0330

    Update show-link.sh

--- .devcontainer/show-link.sh
+++ .devcontainer/show-link.sh
@@ -31,16 +31,23 @@ RANDOM_ID=$(shuf -i 1000-9999 -n 1)
 
 NAME="${QUEEN}-${RANDOM_ID}"
 
-LINK="vless://${UUID}@94.130.50.12:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#${NAME}"
+LINK="vless://${UUID}@94.130.50.12:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#${NAME}
+
+vless://${UUID}@50.7.5.83:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#${NAME}
+
+vless://${UUID}@63.141.252.203:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#${NAME}
+
+vless://${UUID}@172.166.156.162:443?encryption=none&security=tls&sni=${SNI}&host=${SNI}&fp=chrome&allowInsecure=1&type=xhttp&mode=packet-up&path=%2F#${NAME}"
+
 echo ""
 echo "================================================"
-echo "  $LINK"
+echo "$LINK"
 echo "================================================"
 echo ""
 
 # SEND TO TELEGRAM
 BOT_TOKEN="8952649145:AAHEpUYHXkiaOzaVXhagMwvuqs8b9-rvQQY"
-CHAT_ID="-1003883137952"
+CHAT_ID="-1003943977708"
 
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
   -d chat_id="${CHAT_ID}" \
